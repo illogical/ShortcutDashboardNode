1c917a57d61fd203cc0d76d8f1c9a836549b33ca
Matt Moore <thewmoore@gmail.com>
2020-02-09 16:37:37 -0500

    Added fonts. Began configuring server to serve the prod build.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
 var express = require("express");
+const path = require("path");
+
 var router = express.Router();
 
 /* GET home page. */
+router.get("/*", function(req, res, next) {
+  res.sendFile(path.join(__dirname, "../../build", "index.html"));
+});
+
 router.get("/api", function(req, res, next) {
   res.send("API should be running.");
 });
