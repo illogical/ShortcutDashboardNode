a65cb02a620ba52e6e0b1155ab3468bf351596d0
Matt Moore <illogical@users.noreply.github.com>
2021-07-24 22:26:30 -0400

    Remove duplicate configs from db

--- server/routes/config.js
+++ server/routes/config.js
@@ -8,23 +8,22 @@ const engine = new StormDB.localFileEngine("./db.stormdb");
 const db = new StormDB(engine);
 
 const configPath = "../settings/dashboardConfig.json";
+const configName = 'configs';
 
 /* GET  */
 router.get("/", function (req, res, next) {
-  const configsRef = db.get('configs');
-
-  if(db.get(configsRef.value() == null))
+  if(db.get(configName).value() == undefined)
   {
     // TODO: load all .json files from a directory (to support templates)
     const file = fs.readFileSync(path.join(__dirname, configPath));
     const loadedConfig = JSON.parse(file);
 
-    db.default('configs', []);
-    db.get('configs').push({name: "Template", config: loadedConfig});
+    db.set(configName, []);
+    db.get(configName).push({name: "Template", config: loadedConfig});
     db.save();
   }
 
-  const config = configsRef.get(0).get('config');
+  const config = db.get(configName).get(0).get('config');
   console.log("db config", config);
   res.json(config.value());
 });
@@ -34,7 +33,7 @@ router.post("/", function (req, res, next) {
 
   try {
     // fs.writeFileSync(path.join(__dirname, configPath), JSON.stringify(config));
-    db.get('configs').get(0).set('config', config).save();
+    db.get(configName).get(0).set('config', config).save();
   
     res.sendStatus(200);
   } catch (error) {
