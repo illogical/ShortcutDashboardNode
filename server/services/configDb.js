fc216c0364fbe87c3e69d0a6212761545793a13b
Matt Moore <illogical@users.noreply.github.com>
2021-08-01 20:08:24 -0400

    Fix hot reload. Update react-scripts.

--- server/services/configDb.js
+++ server/services/configDb.js
@@ -1,30 +1,25 @@
-const StormDB = require("stormdb");
-const engine = new StormDB.localFileEngine("./db.stormdb");
+const StormDB = require('stormdb');
+const engine = new StormDB.localFileEngine('./server/db/db.stormdb');
 const db = new StormDB(engine);
 
 const hydrateDb = require('../db/hydrateDb.js');
 const configName = 'configs';
 
-const configService = 
-{
-    getConfig()
-    {
-        if(db.get(configName).value() == undefined)
-        {
+const configService = {
+    getConfig() {
+        if (db.get(configName).value() == undefined) {
             hydrateDb(db, configName);
         }
 
         const config = db.get(configName).get(0).get('config');
-        console.log("db config", config);
+        console.log('db config', config);
 
         return config.value();
     },
-    
-    saveConfig(config)
-    {
-        db.get(configName).get(0).set('config', config).save();    
-    }
-}
 
+    saveConfig(config) {
+        db.get(configName).get(0).set('config', config).save();
+    },
+};
 
 module.exports = configService;
